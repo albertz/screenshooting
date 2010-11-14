89d4d66aa5848a690292d73d4de0ad8cc2ed09b9
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-11-14 18:17:36 +0100

    more gen compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
-g++ find-eclipse.cpp -o find-eclipse -I /usr/local/include/opencv -L/usr/local/lib -lopencv_core -lopencv_highgui -lopencv_flann -lopencv_features2d -lopencv_imgproc -lopencv_calib3d
+#!/bin/zsh
+for f in *.cpp; do
+bin=${f/.cpp/.bin}
+echo "$bin .."
+g++ $f -o $bin -I /usr/local/include/opencv -L/usr/local/lib -lopencv_core -lopencv_highgui -lopencv_flann -lopencv_features2d -lopencv_imgproc -lopencv_calib3d
+done
 
